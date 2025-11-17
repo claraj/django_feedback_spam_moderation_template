2c78f7f0e8d9b99727bf7c2653e1649179723407
Clara <10088152+claraj@users.noreply.github.com>
2025-11-17 12:21:18 -0600

    Add optional to the label text for email field in FeedbackForm

--- student_feedback/forms.py
+++ student_feedback/forms.py
@@ -5,3 +5,6 @@ class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ('text', 'email')
+        labels = {
+            'email': 'Email (Optional)'
+        }
